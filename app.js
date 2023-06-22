4380bdb85956931d897f81c08392695bd299a121
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-22 08:03:21 +0300

    Display port number on the coonsole

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express=require('express');
 const app=express();
 //create a port if not exist
 const port=process.env.PORT || 500;
-app.listen(port,()=>console.log(`Server listening at port.${{port}}`));
+app.listen(port,()=>console.log(`Server listening at port.${port}`));
