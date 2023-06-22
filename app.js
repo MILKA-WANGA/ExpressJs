bd53c9184b79fa54f158f63fbfb4135a5b9019f1
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-22 10:04:57 +0300

    Added a format function

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const path=require('path');
 // })
 //Create a middleware
 const logger=(req,res,next)=>{
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}.${moment()}.format`);
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}.${moment().format()}`);
     next();
 }
 //init middleware
