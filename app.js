bf4328a4bd639287ba6e1b314770f14b3841a52f
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-22 08:49:02 +0300

    Created a static folder:public

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app=express();
 //import path
 const path=require('path');
 //create a port if not exist
-app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname,'public','index.html'));
-})
+// app.get("/",(req,res)=>{
+//   res.sendFile(path.join(__dirname,'public','index.html'));
+// })
+//Create a static folder
+app.use(express.static(path.join(__dirname,'public')))
 const port=process.env.PORT || 500;
 app.listen(port,()=>console.log(`Server listening at port ${port}`));
