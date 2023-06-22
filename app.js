6891d4c74ba5f536840997226c93e2aaf347e3b6
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-22 09:51:17 +0300

    Generate url at the console

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path=require('path');
 // })
 //Create a middleware
 const logger=(req,res,next)=>{
-    console.log("Hello");
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
 }
 //init middleware
