9c0ba4807b92a500898b98faa3827ab913b6b06d
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-22 08:11:07 +0300

    Created a route('/')

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const app=express();
 //create a port if not exist
+app.get("/",(req,res)=>{
+  res.send("<h1>Hello ExpressJs</h1>");
+})
 const port=process.env.PORT || 500;
 app.listen(port,()=>console.log(`Server listening at port ${port}`));
