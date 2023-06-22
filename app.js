7dfcb0335cc2deea740d098b9009222df3016c17
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-22 09:07:29 +0300

    Created an api for array:members

--- app.js
+++ app.js
@@ -6,6 +6,29 @@ const path=require('path');
 // app.get("/",(req,res)=>{
 //   res.sendFile(path.join(__dirname,'public','index.html'));
 // })
+//Create an array
+const members=[
+    {
+        "id":1,
+         "name":"Milka",
+         "status":"Active"
+    },
+    {
+        "id":2,
+         "name":"Wanga",
+         "status":"Active"
+    },
+    {
+        "id":3,
+         "name":"Synthia",
+         "status":"Active"
+    }
+];
+//Create an api
+app.get("api/members",(req,res)=>
+{
+    res.json(members);
+})
 //Create a static folder
 app.use(express.static(path.join(__dirname,'public')))
 const port=process.env.PORT || 500;
